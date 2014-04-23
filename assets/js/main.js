fb5c10bc27110658c4c057f8fd2aa71fbf1f58df
Alexander Salas <a.salas@ieee.org>
2014-04-23 02:19:00 -0430

    Improve Track and Playlist count.

--- assets/js/main.js
+++ assets/js/main.js
@@ -27,3 +27,11 @@ jQuery( function( $ ) {
 		}
 	});
 });
+
+jQuery.getJSON( "http://api.soundcloud.com/users/29821826.json?client_id=edddc3c70a1b4e30153a8a49d8d64e8c", function( json ) { 
+	var playlist_count = document.getElementById('playlist_count');
+	if (playlist_count != null) playlist_count.textContent = json.playlist_count;
+	var track_count = document.getElementById('track_count');
+	if (track_count != null) track_count.textContent = json.track_count;
+});
+
